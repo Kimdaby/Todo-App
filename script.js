f62753eb93bff54d6deb97084b60f16c97416dbe
unknown <davidhkim22@gmail.com>
2024-03-15 13:47:21 -0500

    updated

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    var inputField = document.getElementById("inputField");
-    var textValue = inputField.value;
-    var toDoEntry = document.createElement("li");
+    let inputField = document.getElementById("inputField");
+    let textValue = inputField.value;
+    let toDoEntry = document.createElement("li");
     toDoEntry.textContent = textValue;
 
     inputField.value = "";
-    var removebutton = document.createElement("button");
+    let removebutton = document.createElement("button");
     removebutton.textContent = "X";
     removebutton.onclick = function () {
       toDoEntry.remove();
